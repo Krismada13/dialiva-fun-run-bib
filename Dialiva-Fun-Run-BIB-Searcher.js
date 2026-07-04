feb0c5fd6814ceb772a789899092f1d4c74010fa
Krismada13 <100188775+Krismada13@users.noreply.github.com>
2026-07-04 15:38:46 +0700

    Update phone number for Intan Yuliastanti

--- Dialiva-Fun-Run-BIB-Searcher.js
+++ Dialiva-Fun-Run-BIB-Searcher.js
@@ -4,7 +4,7 @@ const DUMMY_REGISTRANTS = [
   { name:"Antonius Abiseka Wiradharma Putra N", bibName:"Abi aja", phone:"085189296181", personalid:"340408070109001", jerseySize:"L", bib:"M-002", regid:"M-002", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Fico Rizki Ikhsan Saputra", bibName:"FICO", phone:"081328488272", personalid:"3403170809890001", jerseySize:"L", bib:"M-003", regid:"M-003", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Ayoedya Java Ratu Adil", bibName:"Ratu Adil", phone:"089653410903", personalid:"3305125312110004", jerseySize:"XS", bib:"F-004", regid:"F-004", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
-  { name:"Intan Yuliastanti", bibName:"INTAN", phone:"085848371110", personalid:"3305215906050002", jerseySize:"S", bib:"F-005", regid:"F-005", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
+  { name:"Intan Yuliastanti", bibName:"INTAN", phone:"085770292201", personalid:"3305215906050002", jerseySize:"S", bib:"F-005", regid:"F-005", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Nadin Ranaa Ayu", bibName:"NADIN RANAA", phone:"081231524656", personalid:"3501046105080001", jerseySize:"M", bib:"F-006", regid:"F-006", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Nurismi", bibName:"Ismi", phone:"082339078095", personalid:"7604046501010006", jerseySize:"L", bib:"F-007", regid:"F-007", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Khoirun Nisa Widyastuti", bibName:"K. Nisa W", phone:"0895401096097", personalid:"3404115708970001", jerseySize:"L", bib:"F-008", regid:"F-008", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
