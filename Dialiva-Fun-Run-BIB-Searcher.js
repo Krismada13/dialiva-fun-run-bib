af5cf36d3c66b642ec152969e3435e0944c035a3
Krismada13 <100188775+Krismada13@users.noreply.github.com>
2026-07-08 12:06:23 +0700

    Add new participant 'Faiz Harinata' to the list

--- Dialiva-Fun-Run-BIB-Searcher.js
+++ Dialiva-Fun-Run-BIB-Searcher.js
@@ -44,6 +44,7 @@ const DUMMY_REGISTRANTS = [
   { name:"Ery Kaustanti", bibName:"ERRIE", phone:"08121501064", personalid:"3494065304900001", jerseySize:"S", bib:"F-042", regid:"F-042", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Alya Zahida", bibName:"ALYA", phone:"087821038075", personalid:"3494065304900001", jerseySize:"S", bib:"F-043", regid:"F-043", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Andreas Chandra Yogyaswara Budiono", bibName:"Andreas Chandra", phone:"081222552282", personalid:"33403030910900002", jerseySize:"", bib:"M-044", regid:"M-044", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
+  { name:"Faiz Harinata", bibName:"FAIZ", phone:"082392854177", personalid:"1303091005060002", jerseySize:"", bib:"M-045", regid:"M-045", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Noorma Bunga Aniri", bibName:"Noorma Aniri", phone:"08557661600", personalid:"3502185610860001", jerseySize:"", bib:"F-046", regid:"F-046", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Nadhira Yasmin Raudha", bibName:"Nadhira Y.R", phone:"08557661600", personalid:"3306095205110003", jerseySize:"", bib:"F-047", regid:"F-047", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
 ];
