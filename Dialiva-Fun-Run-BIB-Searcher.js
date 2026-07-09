0cc5be563b8cd6c4b271beba33e3574a8e74fb7e
Krismada13 <100188775+Krismada13@users.noreply.github.com>
2026-07-09 21:11:00 +0700

    Add new participants to the BIB searcher

--- Dialiva-Fun-Run-BIB-Searcher.js
+++ Dialiva-Fun-Run-BIB-Searcher.js
@@ -61,6 +61,8 @@ const DUMMY_REGISTRANTS = [
   { name:"Imeylda Cahyaning Buana", bibName:"IMEYLDA", phone:"0882007246572", personalid:"3403114912910001", jerseySize:"XL", bib:"F-059", regid:"F-059", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Padmoko Broto Sutanto", bibName:"Broto", phone:"089611203999", personalid:"3402161609900001", jerseySize:"", bib:"M-060", regid:"M-060", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Tsaqif Ega", bibName:"Tsaqif", phone:"085226423700", personalid:"3402162902040003", jerseySize:"", bib:"M-061", regid:"M-061", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
+  { name:"Dea Laksmi Candraningtyas", bibName:"Dea laksmi", phone:"0895422314033", personalid:"3404075805970004", jerseySize:"", bib:"F-062", regid:"F-062", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
+  { name:"Andhika Wignya Paramarta", bibName:"Andhika", phone:"'089652902735", personalid:"3404112704060003", jerseySize:"", bib:"M-062", regid:"M-063", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
 ];
 
 function normalizePhone(p){
