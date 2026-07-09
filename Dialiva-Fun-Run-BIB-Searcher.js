681212c007b3e183cffd31afe126f76f2aa1f142
Krismada13 <100188775+Krismada13@users.noreply.github.com>
2026-07-09 20:40:36 +0700

    Update registrant details for Antonius Abiseka

--- Dialiva-Fun-Run-BIB-Searcher.js
+++ Dialiva-Fun-Run-BIB-Searcher.js
@@ -1,7 +1,7 @@
 // ====== DATA REGISTRANT — Dialiva Fun Run 2026 (dari Google Sheet) ======
 const DUMMY_REGISTRANTS = [
   { name:"Farih Ibnu Zulfa", bibName:"Farizuulll", phone:"081325372200", personalid:"3303180608040001", jerseySize:"L", bib:"M-001", regid:"M-001", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
-  { name:"Antonius Abiseka Wiradharma Putra N", bibName:"Abi aja", phone:"085189296181", personalid:"340408070109001", jerseySize:"L", bib:"M-002", regid:"M-002", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
+  { name:"Antonius Abiseka", bibName:"Abi aja", phone:"082314974749", personalid:"340408070109001", jerseySize:"L", bib:"M-002", regid:"M-002", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Fico Rizki Ikhsan Saputra", bibName:"FICO", phone:"085225463922", personalid:"3403170809890001", jerseySize:"L", bib:"M-003", regid:"M-003", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Ayoedya Java Ratu Adil", bibName:"Ratu Adil", phone:"089653410903", personalid:"3305125312110004", jerseySize:"XS", bib:"F-004", regid:"F-004", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
   { name:"Intan Yuliastanti", bibName:"INTAN", phone:"085770292201", personalid:"3305215906050002", jerseySize:"S", bib:"F-005", regid:"F-005", category:"5K", cot:"00:00:00", status:"Terverifikasi" },
